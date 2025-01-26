4a75c5ec05d9411ab4df753510c09736b31cc698
Akmal Sindarov <asauz777@gmail.com>
2025-01-26 16:37:33 +1000

    Update main.go

--- main.go
+++ main.go
@@ -7,12 +7,7 @@ import (
 )
 
 func main() {
-	// Initialize standard Go html template engine
 	engine := html.New("./views", ".html")
-	// If you want to use another engine,
-	// just replace with following:
-	// Create a new engine with django
-	// engine := django.New("./views", ".django")
 
 	app := fiber.New(fiber.Config{
 		Views: engine,
